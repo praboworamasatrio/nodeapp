3379bb6805a7cddb5f4e16cc5b075266e1e1f476
praboworamasatrio <36912687+praboworamasatrio@users.noreply.github.com>
2023-06-19 16:52:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node change code!\n'
+  const msg = 'Hello Node Testing change code!\n'
   res.end(msg);
 });
 
